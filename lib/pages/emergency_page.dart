37417938a04a229a4f1c9a74bf5b1df9b2ea008a
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2021-07-06 22:34:32 -0300

    Create header and gradient

--- lib/pages/emergency_page.dart
+++ lib/pages/emergency_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:header_button_layout/widgets/header.dart';
 
 class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Página Básica'),
-      ),
+      body: Header(),
     );
   }
 }
